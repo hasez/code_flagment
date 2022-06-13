3e15d2604695c2590bf18f68a30e0eacba447ed9
hasez <hasehaseo@gmail.com>
2022-06-13 23:05:04 +0900

    Fix shellcheck warnings.

--- bash_template.bash
+++ bash_template.bash
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 export PS4='\033[90m+${BASH_SOURCE##*/}:${LINENO}: ${FUNCNAME:+$FUNCNAME: }\033[0m'
 set -Ceuxo pipefail
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
+SCRIPT_DIR=$(
+    cd "$(dirname "$0")"
+    pwd
+)

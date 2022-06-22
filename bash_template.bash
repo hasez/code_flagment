b938325aa1103224dcab1d8858c84853446708a6
hasez <hasehaseo@gmail.com>
2022-06-22 23:10:29 +0900

    Add parameter analysis sample

--- bash_template.bash
+++ bash_template.bash
@@ -5,3 +5,52 @@ SCRIPT_DIR=$(
     cd "$(dirname "$0")"
     pwd
 )
+
+#######
+# paramter analysis
+#######
+DONE_FILE_PATH=""
+param=()
+for OPT in "$@"; do
+    case $OPT in
+    -h | --help)
+        usage
+        exit 0
+        ;;
+    -f | --file)
+        if [[ -z "${2:-}" ]] || [[ "${2}" =~ ^-+ ]]; then
+            echo "ERROR: '${OPT}' option requires an argument."
+            usage
+            exit 1
+        fi
+        DONE_FILE_PATH="${2}"
+        shift 2
+        ;;
+    -- | -)
+        shift 1
+        param+=("$@")
+        break
+        ;;
+    -*)
+        echo "ERROR: Unknown option '${OPT}'"
+        usage
+        exit 1
+        ;;
+    *)
+        if [[ -n "${1:-}" ]] && [[ ! "${1}" =~ ^-+ ]]; then
+            param+=("${1}")
+            shift 1
+        fi
+        ;;
+    esac
+done
+
+if [[ ${#param[@]} != 2 ]]; then
+    echo "ERROR: Invalid number of arguments."
+    usage
+    exit 1
+fi
+
+# 引数取得
+SUBCOMMAND=${param[0]}
+KEY=${param[1]}
